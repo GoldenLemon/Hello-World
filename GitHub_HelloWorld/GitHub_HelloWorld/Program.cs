3e8ae81de1083d8991272fe431a1f19faa1df67c
Dr.Lemonada <wolfchristos@hotmail.com>
2017-01-19 13:13:55 +0100

    Add files via upload

---
+++ GitHub_HelloWorld/GitHub_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloClass helloClass = new HelloClass();
+            helloClass.Run();
+        }
+    }
+}
